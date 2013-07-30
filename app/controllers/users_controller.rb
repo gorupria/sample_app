88726f5ee391354aeb88da563ed1352b78511933
Purushot <battle_g5@yahoo.com>
2013-07-30 15:21:59 +0300

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,16 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
   
+  def create
+    #raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      #handle a successful save.
+      #flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -56,7 +56,60 @@ describe UsersController do
         response.should have_selector('title', :content => "Sign up")
     end
     
-    
+    describe "POST 'create'" do
+      
+      describe "failure" do
+        
+        before(:each) do
+          @attr = {:name => "", :email => "", :password => "", :password_confirmation => ""}
+        end
+        
+        it "should have a right title " do
+          #is in 0803, timestamp: 8:00
+          post :create, :user => @attr
+          response.should have_selector('title', :content => "Sign up")
+        end 
+        
+        it "should render the 'new' page" do
+          post :create, :user => @attr
+          response.should render_template('new')
+        end
+        
+        it "should not create a user" do 
+          lambda do 
+            post :create, :user => @attr
+          end.should_not change(User, :count)
+        end   
+      end
+      
+      describe "success" do
+        
+        before(:each) do
+          @attr = { :name => "New user", :email => "user@example.com", :password => "foobar",
+                    :password_confirmation => "foobar"}
+        end
+        
+        it "should create a user" do
+          lambda do 
+            post :create, :user => @attr
+          end.should change(User, :count).by(1)
+        end
+        
+        it "should redirect to the user show page" do
+          post :create, :user => @attr
+          response.should redirect_to(user_path(assigns(:user)))
+        end
+        
+        it "should have a welcome message" do
+          post :create, :user => @attr
+          #is in 0804, timestamp: 13:00
+          flash[:success].should =~ /welcome to the sample app/i
+        end
+        
+        
+      end
+      
+    end
     
   end
 
